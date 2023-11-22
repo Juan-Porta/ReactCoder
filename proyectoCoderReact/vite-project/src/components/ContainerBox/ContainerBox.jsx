f213883edda94882486c6a74f22512302f720909
Juan Porta <juaniporta2001@hotmail.com>
2023-11-22 19:42:15 -0300

    avance pre entrega 2

--- proyectoCoderReact/vite-project/src/components/ContainerBox/ContainerBox.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Container.module.css"
-
-export const ContainerBox = ({ greeting }) => {
-    return (
-        <div className={styles.container} > { greeting } </div>
-    ) 
-}
--- proyectoCoderReact/vite-project/src/components/NavBar/NavBar.jsx
+++ proyectoCoderReact/vite-project/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,24 @@
-import { CartWidget } from "../CartWidget/CartWidget"
+import { Link } from "react-router-dom";
+import { CartWidget } from "../CartWidget/CartWidget";
 
 export const NavBar = () => {
   return (
     <nav className="d-flex justify-content-around p-4">
-        <img src="/img/logo.png" alt="" style={{width: "160px", height: "100px"}}/>
+        <img src="/img/logo.png" alt="" style={{width: "190px", height: "100px"}}/>
         <div>
-        <button className="btn btn-success mx-2">Hogar</button>
-        <button className="btn btn-success mx-2">Prendas</button>
-        <button className="btn btn-success mx-2">Contactos</button>
-      </div>
-      <CartWidget/>
-    </nav>
-  )
-}
+          <Link to="/">
+            <button className="btn btn-success mx-2">Todos</button>
+          </Link>
+          <Link to="/category/calzados">
+            <button className="btn btn-success mx-2">Calzado</button>
+          </Link>
+          <Link to="/category/ropa">
+            <button className="btn btn-success mx-2">Remeras</button>
+          </Link>
+        </div>
+          <Link to="/cart">
+            <CartWidget />
+          </Link>
+        </nav>
+  );
+};
